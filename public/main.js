fd2947b0a9e4edea8b60baed3c016c263feda741
zerop <zerop.development@gmail.com>
2022-08-24 02:15:26 +0300

    add basic math functions

---
+++ public/main.js
@@ -0,0 +1,13 @@
+"use strict";
+function sum(a, b) {
+    return a + b;
+}
+function min(a, b) {
+    return a - b;
+}
+function div(a, b) {
+    return a / b;
+}
+function mult(a, b) {
+    return a * b;
+}
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+
+
+
+
+function sum (a: number, b: number): number {
+    return a + b
+}
+
+function min (a: number, b: number): number {
+    return a - b
+}
+
+function div (a: number, b: number): number {
+    return a / b
+}
+
+function mult (a: number, b: number): number {
+    return a * b
+}
