8ce6bfe28d54e00b56b012687ff607da287b3c54
zerop <zerop.development@gmail.com>
2022-08-24 02:58:47 +0300

    add description functions 0f log and root extractio

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 "use strict";
+/*    */
 function sum(a, b) {
     return a + b;
 }
@@ -10,4 +11,34 @@ function div(a, b) {
 }
 function mult(a, b) {
     return a * b;
+}
+/* basic math functon  */
+/*    */
+function squere(a) {
+    return a * a;
+}
+function cube(a) {
+    return a * a * a;
+}
+/*    */
+function exp(num, deg) {
+    let res = 1;
+    for (let i = 0; i < deg; i++) {
+        res = res * num;
+    }
+    return res;
+}
+// логорифм - функция нахождения степени основания LOG  2   8 = 3 | LOG 2 8 = 2^3
+//                                                     осн арг        
+function log(root, arg) {
+    for (let deg = 0; deg < Infinity; deg++) {
+        if (exp(root, deg) === arg) {
+            return deg;
+        }
+    }
+}
+// извление корня =- функция получения основания 
+function root(exp, num) {
+    let r;
+    return 1;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 
-
-
-
+/*    */
 function sum (a: number, b: number): number {
     return a + b
 }
@@ -16,4 +14,50 @@ function div (a: number, b: number): number {
 
 function mult (a: number, b: number): number {
     return a * b
+}
+/* basic math functon  */
+
+
+/*    */
+function squere (a: number): number {
+    return a * a
+}
+
+function cube (a: number): number {
+    return a * a * a
+}
+/*    */
+
+function exp (num: number, deg: number): number {
+
+    let res: number = 1
+
+    for (let i = 0; i < deg; i++) {
+        res = res * num
+    }
+
+    return res
+}
+
+
+// логорифм - функция нахождения степени основания LOG  2   8 = 3 | LOG 2 8 = 2^3
+//                                                     осн арг        
+function log (root: number, arg: number) {
+
+    for (let deg = 0; deg < Infinity; deg++) {
+
+        if (exp(root, deg) === arg) {
+            return deg
+        }
+
+    }
+
+}
+// извление корня =- функция получения основания 
+function root (exp: number, num: number): number {
+
+    let r: number
+
+
+    return 1
 }
